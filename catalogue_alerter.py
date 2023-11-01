54e79667e59a208fafefdf683901cf46d7293111
Ethal Askander <Ethal.Askander@anu.edu.au>
2023-11-01 23:53:23 +1100

    feat: scrape_woolworths_catalogue works in command line. bug: scrape_coles_catalogue only scrapes last page of the page list provided. fix: error when trying to get button to open Woolworths catalogue

--- catalogue_alerter.py
+++ catalogue_alerter.py
@@ -55,35 +55,7 @@ def read_alert_items(file_name: str ='items.txt'):
 
 async def scrape_coles_catalogue(browser, upcoming: bool = True, catalogue_pages: list[str] = ['page1', 'page2', 'page3']):
     '''
-    Scrapes pages from the coles catalogue (if it is available).
-    '''
-    # Create a new page and go to the cole catalogue website
-    page = await browser.newPage()
-    await page.goto("https://www.coles.com.au/catalogues")
-
-    # Open up the catalogue TODO: Test when upcoming is false. TODO: Handle case of next week's catalogue being not available
-    catalogue_button = await page.waitForXPath('//a[@aria-label="View next week\'s catalogue"]') if upcoming else await page.waitForXPath('//a[@aria-label="View this week\'s catalogue"]')
-    catalogue_url = await page.evaluate('(element) => element.getAttribute("href")', catalogue_button)
-    await page.goto('https://www.coles.com.au' + catalogue_url)
-
-    # Retrieve item titles (got help from ChatGPT for code below)
-    await page.waitForXPath('//a[@aria-label="Specials of the Week"]') # Wait for the anchors on the pages to load
-    titles = await page.evaluate('''() => {
-    const titles = [];
-    const elements = document.querySelectorAll(\''''+ ''.join(f'li.{cp}, '.format(cp) for cp in catalogue_pages)[:-2] + ''' .slide-content.objloaded a');
-    elements.forEach((element) => {
-        if (element instanceof HTMLAnchorElement) {
-            titles.push(element.title);
-        }
-    });
-    return titles;
-    }''')
-
-    return titles
-
-async def scrape_coles_catalogue(browser, upcoming: bool = True, catalogue_pages: list[str] = ['page1', 'page2', 'page3']):
-    '''
-    Scrapes pages from the woolworths catalogue (if it is available).
+    Scrapes pages from the Coles catalogue (if it is available).
     '''
     # Create a new page and go to the cole catalogue website
     page = await browser.newPage()
@@ -107,15 +79,69 @@ async def scrape_coles_catalogue(browser, upcoming: bool = True, catalogue_pages
 
     # Retrieve item titles (got help from ChatGPT for code below)
     await page.waitForXPath('//a[@aria-label="Specials of the Week"]') # Wait for the anchors on the pages to load
+    # FIX: Only captures anchors on last page
     titles = await page.evaluate('''() => {
     const titles = [];
-    const elements = document.querySelectorAll(\''''+ ''.join(f'li.{cp}, '.format(cp) for cp in catalogue_pages)[:-2] + ''' .slide-content.objloaded a');
+    const elements = document.querySelectorAll(\''''+ ''.join(f'li.{cp}, ' for cp in catalogue_pages)[:-2] + ''' .slide-content.objloaded a');
     elements.forEach((element) => {
         if (element instanceof HTMLAnchorElement) {
             titles.push(element.title);
         }
     });
     return titles;
+    }''')
+
+    return titles
+
+async def scrape_woolworths_catalogue(browser, postcode: str, upcoming: bool = True, catalogue_pages: list[str] = ['page0', 'page1', 'page2']):
+    '''
+    Scrapes pages from the woolworths catalogue (if it is available).
+    '''
+    # Create a new page and go to the woolworths catalogue website
+    page = await browser.newPage()
+    await page.goto("https://www.woolworths.com.au/shop/catalogue")
+
+    # Provide the postcode and open the first location that pops up
+    try:
+        catalogue_location_autocomplete = await page.waitForXPath('//input[@id="wx-digital-catalogue-autocomplete"]')
+        await catalogue_location_autocomplete.type(postcode)
+        catalogue_location_first_autocomplete = await page.waitForXPath('//li[@id="wx-digital-catalogue-autocomplete__option--0"]')
+        await catalogue_location_first_autocomplete.click()
+    except pyppeteer.errors.TimeoutError as timeout_error:
+        print(f'Failed to retrieve elements to select the Woolworths location using the provided postcode within 30 seconds')
+        print(timeout_error)
+        return []
+    except Exception as exception:
+        print('Something went wrong when trying to retrieve elements to select the Woolworths location using the provided postcode')
+        print(exception)
+        return []
+
+    # TODO: Test and accomodate upcoming catalogue
+    # TODO: Handle exception scenarios
+    # FIX: Getting error 'Error: Node is either not visible or not an HTMLElement'
+    # Open up the catalogue
+    catalogue_button = await page.waitForXPath('//a[@class="read-catalogue"]') # Element is dynamically loaded in so we must wait for it to become visible.
+    await catalogue_button.click()
+
+    # Retrieve item titles (got help from ChatGPT for code below)
+    await page.waitForXPath('//div[@id="sf-catalogue"]') # Wait for the anchors on the pages to load
+    titles = await page.evaluate('''() => {
+        const titles = [];
+        const pageNames = [''' + ''.join(f'"{cp}", ' for cp in catalogue_pages)[:-2] + '''];
+        const allElements = [];
+
+        pageNames.forEach(pageName => {
+        const selector = `li.${pageName} .slide-content.objloaded a`;
+        const elements = Array.from(document.querySelectorAll(selector));
+        allElements.push(...elements);
+        });
+
+        allElements.forEach((element) => {
+            if (element instanceof HTMLAnchorElement && element.title != '') {
+                titles.push(element.title);
+            }
+        });
+        return titles;
     }''')
 
     return titles
@@ -133,9 +159,15 @@ async def main():
     try:
         # TODO: Make the executable path an input that the user can provide
         browser = await pyppeteer.launch(executablePath="C:\Program Files\Google\Chrome\Application\chrome.exe", headless=True)
-        titles = await scrape_coles_catalogue(browser, upcoming=False)
-        matches = match_items(alert_items, titles)
-        print(f'Coles matches: {matches}')
+        coles_catalogue_items = await scrape_coles_catalogue(browser, upcoming=False)
+        woolworths_catalogue_items = await scrape_woolworths_catalogue(browser, postcode='TODO', upcoming=False)
+        coles_matches = match_items(alert_items, coles_catalogue_items)
+        woolworths_matches = match_items(alert_items, woolworths_catalogue_items)
+
+        # Print results
+        print(f'woolworths: {woolworths_catalogue_items}')
+        print(f'Woolworths matches: {woolworths_matches}')
+        print(f'Coles matches: {coles_matches}')
     except Exception as e:
         print("Error:", str(e))
     finally:
